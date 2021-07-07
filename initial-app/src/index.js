251261b56726d15b9652851f3e58eb84bf086254
Antonio Ladeia <contato@antonioladeia.com>
2021-07-07 19:48:33 -0300

    criando um novo componente e passando parâmetros para ele

--- initial-app/src/index.js
+++ initial-app/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
+import App2 from './components/App2';
 
 ReactDOM.render(
-  <App />,
+  <div>
+    <App2  title="lala" text="lolo">
+      Um novo conteúdo  
+    </App2>
+
+    <App2  title="dadada" text="dodo">
+    Um novo componente
+    </App2>
+  </div>,
   document.getElementById('root')
 );
