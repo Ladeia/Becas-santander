db1ee52a1e4173379e05dda6876b5259dbd08803
Antonio Ladeia <contato@antonioladeia.com>
2021-07-07 19:26:23 -0300

    criando o primeiro componente estático

--- initial-app/src/App.js
+++ initial-app/src/App.js
@@ -4,7 +4,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <p>Paragrafo de teste</p>
+        <p>Segundo parágrafo de teste</p>
+        <p>{ new Date().toLocaleDateString("pt-BR") }</p>
       </div>
     );
   }
